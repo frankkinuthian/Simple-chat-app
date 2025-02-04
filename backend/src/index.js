4dba8bac0bdf5f1ea149215dd57c49dc9e70f3b6
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-05 02:54:46 +0300

    Added Pages, SignUp Page complete and Auth pattern done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "../lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "../routes/message.route.js";
+import cors from "cors";
 
 // app config
 const app = express();
@@ -15,6 +16,12 @@ const PORT = process.env.PORT || 5001;
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // api routes
 
