96db0f04035e7b7e31c753368cc9d3c3e50e8835
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-05 03:55:45 +0300

    Login Page UI done, added Login func to store
    * Similar UI outline to signup
    * Tested api and response
    * Logout successfully clears jwt token from local

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -44,4 +44,25 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  login: async (data) => {
+    // Initiates the login process by setting the isLoggingIn state to true
+    set({ isLoggingIn: true });
+    try {
+      // Attempts to send a login request to the server and awaits the response
+      const res = await axiosInstance.post("/auth/login", data);
+      // Updates the authUser state with the data received from the server upon successful login
+      set({ authUser: res.data });
+      // Displays a success message toast to notify the user of successful login
+      toast.success("Logged in successfully");
+
+      // Calls the connectSocket method to establish a socket connection after successful login
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      // Ensures that the isLoggingIn state is set back to false after the login attempt, regardless
+      set({ isLoggingIn: false });
+    }
+  },
 }));
