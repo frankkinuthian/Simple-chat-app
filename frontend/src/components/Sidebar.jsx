621f282404aeba312669ca5a1b83f2aecc2446c8
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-08 14:46:08 +0300

    Moved online users array

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { User } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Sidebar = () => {
   const { users, isUsersLoading, selectedUser, setSelectedUser, getUsers } =
     useChatStore();
-  const onlineUsers = [];
-
+   const { onlineUsers } = useAuthStore();
   useEffect(() => {
     getUsers();
   }, [getUsers]);
