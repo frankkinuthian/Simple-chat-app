f28b723601f715180953ab08720d2ffada51e179
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-08 11:17:36 +0300

    Update App.jsx
    Added state to update entire theme UI, proceeding to build Settings theme page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,33 +9,48 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore(); //get theme state from zustand
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({ authUser })
+  console.log({ authUser });
 
   // Loading state
   if (isCheckingAuth && !authUser) {
-    return <div className="flex items-center justify-center h-screen">
-      <Loader className="size-10 animate-spin" />
-    </div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
   }
 
   return (
-    <div data-theme="corporate">
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
 
       <Toaster />
