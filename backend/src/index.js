ba6b3eeb28c21337c41c58bad12d284fd69486e9
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-14 16:26:10 +0300

    Made proper changes to the file structure of backend folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import authRoutes from "../routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "../lib/db.js";
+import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-import messageRoutes from "../routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 
 // app config
