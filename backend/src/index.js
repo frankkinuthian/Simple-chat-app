995b7408b56c3601faa0517debd40a37eb9bae2a
Frank Kinuthia <frank.kinuthia@outlook.com>
2025-02-14 18:44:34 +0300

    Preparation for development

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,9 +35,10 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend", "dist")));
 
-  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-
 
 // listen
 server.listen(PORT, () => {
